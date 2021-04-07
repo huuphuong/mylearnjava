3f67ef1c5cf3262e88bd1f6d91056b38f86d5792
Huu Phuong <huu.phuong195@gmail.com>
2021-04-07 22:25:46 +0700

    note

--- CollectionFramework/JavaLinkedList.java
+++ CollectionFramework/JavaLinkedList.java
@@ -1,6 +1,16 @@
 package CollectionFramework;
 
 public class JavaLinkedList {
+    /**
+     * Kế thừa AbtractSequentialList và implement List, Queue Interface
+     * Sử dụng cấu trúc danh sách liên kết đôi Doubly để lưu trữ phần tử
+     * Đặc điểm:
+     *  + Có thể chứa các phần tử trùng lặp
+     *  + Duy trì thứ tự của phần tử thêm vào
+     *  + Bất đồng bộ
+     *  + Thao tác nhanh vì ko phải đánh lại index nếu có hành động xóa phần tử
+     *  + Có thể sử dụng là 1 list (danh sách), ngăn xếp (stack) hoặc hàng đợi (queued)
+     */
     public static void main(String[] args) {
         
     }
