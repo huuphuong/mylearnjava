03c967fb1a56fe4f3a20999c9691ab71906ed3ed
Phuong Huu <huu.phuong195@gmail.com>
2021-04-12 10:59:42 +0700

    ajava

--- JavaInner/AnonymousInner.java
+++ JavaInner/AnonymousInner.java
@@ -1,5 +1,8 @@
 package JavaInner;
 
+/**
+ * Cách này lớp inner sẽ ghi đè 1 phương thức của outer, outer nên là 1 abtracts class hoặc 1 interface
+ */
 abstract class Person {
     abstract void eat();
 }
@@ -7,6 +10,7 @@ abstract class Person {
 public class AnonymousInner {
     public static void main(String[] args) {
         Person person = new Person(){
+            // Ghi đè method eat của outer Person
             void eat() {
                 System.out.println("Eat rau xanh");
             }
